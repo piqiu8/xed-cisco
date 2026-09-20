8355361d150abeb93a8534a297f6608957121f38
piqiu8 <anakin.hx+Github@gmail.com>
2026-09-20 21:12:56 +0800

    Update Main.kt

--- app/src/main/java/com/rk/demo/Main.kt
+++ app/src/main/java/com/rk/demo/Main.kt
@@ -1,36 +1,42 @@
 package com.rk.demo
 
+import android.content.res.Resources
 import androidx.annotation.Keep
 import com.rk.extension.ExtensionAPI
 import com.rk.extension.ExtensionContext
-import com.rk.utils.toast
-
-@Keep
-@Suppress("unused")
-class Main(context: ExtensionContext) : ExtensionAPI(context) {
-    override fun onLoad() {
-        // Called only once when extension is loaded
-        // NOTE: Might get called even before the main activity or any UI context is available
-
-        val fileResolver = AssetsFileResolver(context.assets)
-        FileProviderRegistry.getInstance().addFileProvider(fileResolver)
-        GrammarRegistry.getInstance().loadGrammars("languages.json")
-        FileTypeManager.register(CiscoLanguage(context.resources))
-
-        // The extension will stay in memory until the app is killed by system or force stopped
-        // For more information, view the official documentation: https://xed-editor.github.io/Xed-Docs/docs/extensions/
-    }
-
-    override fun onDispose() {
-        // Any running tasks by your extension should be stopped after this function call
-        // NOTE: Make sure to also unregister all commands, language servers, etc.
-    }
-}
+import com.rk.extension.filetype.FileType
+import com.rk.extension.filetype.FileTypeManager
+import com.rk.extension.grammar.GrammarRegistry
+import com.rk.extension.provider.AssetsFileResolver
+import com.rk.extension.provider.FileProviderRegistry
+import com.rk.extension.ui.Icon
 
 class CiscoLanguage(resources: Resources) : FileType {
-    override val extensions = listOf("ios")
+    override val extensions = listOf("ios", "cisco")
     override val textmateScope = "text.cisco"
     override val icon = Icon.ExternalResourceIcon(R.drawable.ic_language, resources)
     override val name = "cisco"
     override val title = "Cisco IOS"
+}
+
+@Keep
+@Suppress("unused")
+class Main(context: ExtensionContext) : ExtensionAPI(context) {
+    private var fileResolver: AssetsFileResolver? = null
+    private var ciscoLanguage: CiscoLanguage? = null
+    override fun onLoad() {
+        fileResolver = AssetsFileResolver(context.assets)
+        FileProviderRegistry.getInstance().addFileProvider(fileResolver!!)
+        GrammarRegistry.getInstance().loadGrammars("languages.json")
+        ciscoLanguage = CiscoLanguage(context.resources)
+        FileTypeManager.register(ciscoLanguage!!)
+    }
+    override fun onDispose() {
+        fileResolver?.let {
+            FileProviderRegistry.getInstance().removeFileProvider(it)
+        }
+        ciscoLanguage?.let {
+            FileTypeManager.unregister(it)
+        }
+    }
 }
