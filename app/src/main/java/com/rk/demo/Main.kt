b834b39bcd03b63d61db7dde201a0a62787a2d95
piqiu8 <anakin.hx+Github@gmail.com>
2026-09-20 21:08:22 +0800

    Update Main.kt

--- app/src/main/java/com/rk/demo/Main.kt
+++ app/src/main/java/com/rk/demo/Main.kt
@@ -12,9 +12,10 @@ class Main(context: ExtensionContext) : ExtensionAPI(context) {
         // Called only once when extension is loaded
         // NOTE: Might get called even before the main activity or any UI context is available
 
-        val res = context.resources
-        val text = res.getString(R.string.hello_world)
-        toast(text)
+        val fileResolver = AssetsFileResolver(context.assets)
+        FileProviderRegistry.getInstance().addFileProvider(fileResolver)
+        GrammarRegistry.getInstance().loadGrammars("languages.json")
+        FileTypeManager.register(CiscoLanguage(context.resources))
 
         // The extension will stay in memory until the app is killed by system or force stopped
         // For more information, view the official documentation: https://xed-editor.github.io/Xed-Docs/docs/extensions/
@@ -24,4 +25,12 @@ class Main(context: ExtensionContext) : ExtensionAPI(context) {
         // Any running tasks by your extension should be stopped after this function call
         // NOTE: Make sure to also unregister all commands, language servers, etc.
     }
+}
+
+class CiscoLanguage(resources: Resources) : FileType {
+    override val extensions = listOf("ios")
+    override val textmateScope = "text.cisco"
+    override val icon = Icon.ExternalResourceIcon(R.drawable.ic_language, resources)
+    override val name = "cisco"
+    override val title = "Cisco IOS"
 }
