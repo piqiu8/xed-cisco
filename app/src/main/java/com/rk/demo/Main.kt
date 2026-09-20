8595ce396c194fea3a0db51b4b6154582c5fcf32
piqiu8 <anakin.hx+Github@gmail.com>
2026-09-20 21:21:05 +0800

    Update Main.kt

--- app/src/main/java/com/rk/demo/Main.kt
+++ app/src/main/java/com/rk/demo/Main.kt
@@ -12,29 +12,49 @@ import com.rk.extension.provider.FileProviderRegistry
 import com.rk.extension.ui.Icon
 
 class CiscoLanguage(resources: Resources) : FileType {
+
     override val extensions = listOf("ios", "cisco")
+
     override val textmateScope = "text.cisco"
-    override val icon = Icon.ExternalResourceIcon(R.drawable.ic_language, resources)
+
+    override val icon =
+        Icon.ExternalResourceIcon(R.drawable.ic_language, resources)
+
     override val name = "cisco"
+
     override val title = "Cisco IOS"
 }
 
 @Keep
 @Suppress("unused")
 class Main(context: ExtensionContext) : ExtensionAPI(context) {
+
     private var fileResolver: AssetsFileResolver? = null
+
     private var ciscoLanguage: CiscoLanguage? = null
+
     override fun onLoad() {
+
         fileResolver = AssetsFileResolver(context.assets)
-        FileProviderRegistry.getInstance().addFileProvider(fileResolver!!)
-        GrammarRegistry.getInstance().loadGrammars("languages.json")
+
+        FileProviderRegistry.getInstance()
+            .addFileProvider(fileResolver!!)
+
+        GrammarRegistry.getInstance()
+            .loadGrammars("languages.json")
+
         ciscoLanguage = CiscoLanguage(context.resources)
+
         FileTypeManager.register(ciscoLanguage!!)
     }
+
     override fun onDispose() {
+
         fileResolver?.let {
-            FileProviderRegistry.getInstance().removeFileProvider(it)
+            FileProviderRegistry.getInstance()
+                .removeFileProvider(it)
         }
+
         ciscoLanguage?.let {
             FileTypeManager.unregister(it)
         }
